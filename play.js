daffae5976a7a150be887587c6cd918cbd196191
Cedric.gauthier <cidgaut@gmail.com>
2024-01-17 21:06:02 -0500

    required the functions I transfered to input file. kept note of two different ways to request the exported files. For now I will keep it as reference

--- play.js
+++ play.js
@@ -1,40 +1,8 @@
 
 const connectKey = require("./client"); //we assign a variable to the file we require to then request the object using a key
-
+const { setupInput } = require("./input"); //another way to require a exported "object"
 //
 console.log("Connecting ...");
 connectKey.connect(); //this is where we take the  => file we require via value => dot => name of the object/key exported from said file
 
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function (key) { //user input
-    if (key === "\u0003") { //key ctrl + c to log out
-      process.exit();
-    }
-
-    /*if(key === "w") { // wsad move up
-      conn.write("Move: up");
-    }
-
-    if(key === "s") { // wsad move up
-      conn.write("Move: down");
-    }
-    
-    if(key === "a") { // wsad move up
-      conn.write("Move: left");
-    }
-
-    if(key === "r") { // wsad move up
-      conn.write("Move: right");
-    }
-  };*/
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-}
+setupInput(); //another way to call the function
