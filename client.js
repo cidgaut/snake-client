f440d6e83823b336db4d3937a20e6f59aa5923ad
Cedric.gauthier <cidgaut@gmail.com>
2024-01-17 19:32:03 -0500

    added my name to the snake

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require("net"); //needed for line 5
+
+  // establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server"); //changed previous message
+  
+    conn.write("Name: CID"); //still witthin the conn.on function. Won't work outside.
+  });
+
+  
+
+  return conn;
+};
+
+
+module.exports = {connect};
+
+//had touble with this one 
+//there is a connect function that we turn into an object once it's in module.exports
