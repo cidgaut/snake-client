66db5f8b0e27f1d3a081130f483252027bef3dae
Cedric.gauthier <cidgaut@gmail.com>
2024-01-17 20:32:11 -0500

    added setupInput and handleuserinput, incomplete

--- play.js
+++ play.js
@@ -6,4 +6,15 @@ console.log("Connecting ...");
 connectKey.connect(); //this is where we take the  => file we require via value => dot => name of the object/key exported from said file
 
 
-//
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
+};
